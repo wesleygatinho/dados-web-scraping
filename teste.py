6639c9cbb37aeb76c35691d618485b1a6795435a
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-10-24 14:28:13 -0300

    correção dados

--- teste.py
+++ teste.py
@@ -13,12 +13,12 @@ driver = webdriver.Chrome(service=webdriver.chrome.service.Service(driver_path),
 
 try:
     # Acessar o site
-    url = "https://sigaa.ufma.br/sigaa/public/curso/concluidos_curso.jsf?lc=pt_BR&id=10816685"
+    url = "https://sigaa.ufma.br/sigaa/public/curso/monografias_curso.jsf?lc=pt_BR&lc=pt_BR&id=10816685"
     driver.get(url)
 
     # Clicar no botão de busca
     wait = WebDriverWait(driver, 20)
-    search_button = wait.until(EC.element_to_be_clickable((By.ID, "form:buscarTurmas")))
+    search_button = wait.until(EC.element_to_be_clickable((By.ID, "form:buscar")))
     search_button.click()
 
     # Aguarde a tabela carregar
@@ -33,12 +33,15 @@ try:
     for row in rows[1:-1]:  # Ignorar cabeçalho e última linha
         cols = row.find_elements(By.TAG_NAME, "td")
         if len(cols) >= 2:
-            matricula = cols[0].text.strip()
-            nome = cols[1].text.strip()
-            data.append([matricula, nome])
+            ano = cols[0].text.strip()
+            date = cols[1].text.strip()
+            aluno = cols[2].text.strip()
+            orientador = cols[3].text.strip()
+            curso = cols[4].text.strip()
+            data.append([ano, date, aluno, orientador, curso])
 
     # Converter para DataFrame
-    df = pd.DataFrame(data, columns=["Matrícula", "Aluno"])
+    df = pd.DataFrame(data, columns=["Ano", "Data", "Aluno", "Orientador", "Curso"])
 
     # Exibir os dados no console de forma tabular
     print(df.to_string(index=False))
